7db06ae6a1ba066a68d95d7e3dc4ccd638bcda38
shameer <k164030@nu.edu.pk>
2019-07-12 00:41:38 +0500

    checking

--- Project/src/main/java/com/Hotel/io/Project/ProjectApplication.java
+++ Project/src/main/java/com/Hotel/io/Project/ProjectApplication.java
@@ -62,12 +62,33 @@ public class ProjectApplication implements CommandLineRunner {
 		userService.createUser(user2, userRoles);
 	}
 
+
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	  TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	      @Override
+	      protected void postProcessContext(Context context) {
+	        SecurityConstraint securityConstraint = new SecurityConstraint();
+	        securityConstraint.setUserConstraint("CONFIDENTIAL");
+	        SecurityCollection collection = new SecurityCollection();
+	        collection.addPattern("/*");
+	        securityConstraint.addCollection(collection);
+	        context.addConstraint(securityConstraint);
+	      }
+	    };
+	  
+	  tomcat.addAdditionalTomcatConnectors(redirectConnector());
+	  return tomcat;
+	}
+
 	private Connector redirectConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
+	  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	  connector.setScheme("http");
+	  connector.setPort(8080);
+	  connector.setSecure(false);
+	  connector.setRedirectPort(8443);
+	  
+	  return connector;
 	}
 }
