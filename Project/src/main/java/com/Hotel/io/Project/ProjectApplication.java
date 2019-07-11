6ae1ff1de2ed7ded2253ecd8a3faed1eff72716d
shameer <k164030@nu.edu.pk>
2019-07-12 00:59:52 +0500

    checking2

--- Project/src/main/java/com/Hotel/io/Project/ProjectApplication.java
+++ Project/src/main/java/com/Hotel/io/Project/ProjectApplication.java
@@ -13,6 +13,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -65,30 +79,28 @@ public class ProjectApplication implements CommandLineRunner {
 
 
 	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-	  TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-	      @Override
-	      protected void postProcessContext(Context context) {
-	        SecurityConstraint securityConstraint = new SecurityConstraint();
-	        securityConstraint.setUserConstraint("CONFIDENTIAL");
-	        SecurityCollection collection = new SecurityCollection();
-	        collection.addPattern("/*");
-	        securityConstraint.addCollection(collection);
-	        context.addConstraint(securityConstraint);
-	      }
-	    };
-	  
-	  tomcat.addAdditionalTomcatConnectors(redirectConnector());
-	  return tomcat;
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
 	}
 
 	private Connector redirectConnector() {
-	  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-	  connector.setScheme("http");
-	  connector.setPort(8080);
-	  connector.setSecure(false);
-	  connector.setRedirectPort(8443);
-	  
-	  return connector;
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 }
